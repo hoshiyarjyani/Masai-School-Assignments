a7fc6ccb1914dacddb7ce29c215eee156b9348f9
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-10-25 15:56:56 +0530

    masai addmision data base With JSON

--- unit-2/sprint-3/day-4/assignments/Masai Addmission database/admitted.js
+++ unit-2/sprint-3/day-4/assignments/Masai Addmission database/admitted.js
@@ -2,22 +2,21 @@
 
 var acceptArr = JSON.parse(localStorage.getItem("admission-accepted")) || [];
 
-displayTable();
-function displayTable() {
-  for (let i = 0; i < acceptArr.length; i++) {
+function displayTable(data) {
+   data.forEach(function(element){
     var row = document.createElement("tr");
     var td1 = document.createElement("td");
-    td1.innerText = acceptArr[i].name;
+    td1.innerText = element.name;
     var td2 = document.createElement("td");
-    td2.innerText = acceptArr[i].email;
+    td2.innerText = element.email;
     var td3 = document.createElement("td");
-    td3.innerText = acceptArr[i].course;
+    td3.innerText = element.course;
     var td4 = document.createElement("td");
-    td4.innerText = acceptArr[i].gender;
+    td4.innerText = element.gender;
     var td5 = document.createElement("td");
-    td5.innerText = acceptArr[i].mobile;
-    
+    td5.innerText = element.mobile;
     row.append(td1, td2, td3, td4, td5);
     document.querySelector("tbody").append(row);
+   });
   }
-}
+displayTable(acceptArr); 
--- unit-2/sprint-3/day-4/assignments/Masai Addmission database/rejected.js
+++ unit-2/sprint-3/day-4/assignments/Masai Addmission database/rejected.js
@@ -1,22 +1,21 @@
 // All the Code for the Rejected page goes here
 var rejectArr = JSON.parse(localStorage.getItem("admission-rejected")) || [];
 
-displayTable();
-function displayTable() {
-  for (let i = 0; i < rejectArr.length; i++) {
-    var row = document.createElement("tr");
-    var td1 = document.createElement("td");
-    td1.innerText = rejectArr[i].name;
-    var td2 = document.createElement("td");
-    td2.innerText = rejectArr[i].email;
-    var td3 = document.createElement("td");
-    td3.innerText = rejectArr[i].course;
-    var td4 = document.createElement("td");
-    td4.innerText = rejectArr[i].gender;
-    var td5 = document.createElement("td");
-    td5.innerText = rejectArr[i].mobile;
-
-    row.append(td1, td2, td3, td4, td5);
-    document.querySelector("tbody").append(row);
-  }
-}
+function displayTable(data) {
+  data.forEach(function(element){
+   var row = document.createElement("tr");
+   var td1 = document.createElement("td");
+   td1.innerText = element.name;
+   var td2 = document.createElement("td");
+   td2.innerText = element.email;
+   var td3 = document.createElement("td");
+   td3.innerText = element.course;
+   var td4 = document.createElement("td");
+   td4.innerText = element.gender;
+   var td5 = document.createElement("td");
+   td5.innerText = element.mobile;
+   row.append(td1, td2, td3, td4, td5);
+   document.querySelector("tbody").append(row);
+  });
+ }
+displayTable(rejectArr);
