4cc4fe625b5e8144466d64e89048ef195aa0e840
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-12-24 21:07:46 +0530

    Week day using if condition  print in java

---
+++ dsa401/UseOf_If_Condition.java
@@ -0,0 +1,79 @@
+public class UseOf_If_Condition {
+    public static void weekday(int num) {
+        // write your code here
+        if (num == 1) {
+            System.out.println("Monday");
+        } else if (num == 2) {
+            System.out.println("Tuesday");
+        } else if (num == 3) {
+            System.out.println("Wednesday");
+        } else if (num == 4) {
+            System.out.println("Thursday");
+        } else if (num == 5) {
+            System.out.println("Friday");
+        } else if (num == 6) {
+            System.out.println("Saturday");
+        } else if (num == 7) {
+            System.out.println("Sunday");
+        }
+    }
+
+    public static void main(String[] args) {
+        weekday(5);
+    }
+
+}
+
+// Week Day -26:54:7
+// Description
+
+// You are given a number, stored in the variable, with the namenum
+
+// Print the required value according to the following conditions
+
+// if
+
+// num = 1, print "Monday"
+
+// num = 2, print "Tuesday"
+
+// num = 3, print "Wednesday"
+
+// num = 4, print "Thursday"
+
+// num = 5, print "Friday"
+
+// num = 6, print "Saturday"
+
+// num = 7, print "Sunday"
+
+// It is guaranteed, that the value stored in num, will be in the range from1 to
+// 7
+
+// Input
+// The first and the only line of the input contains the value stored innum
+
+// Output
+// Print the value according to the condition given in the problem statement
+
+// Sample Input 1
+
+// 2
+// Sample Output 1
+
+// Tuesday
+// Hint
+
+// In the sample test case, the value stored innum = 2
+
+// Therefore, according to the condition given in the problem statement, the
+// answer will beTuesday
+
+// Language:
+
+// Theme:
+// Font Size: 20
+
+//
+// Test against custom input
+// Status Accepted
