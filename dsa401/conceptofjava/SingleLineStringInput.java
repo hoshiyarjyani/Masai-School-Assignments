502a08d6e873a296c25268bcb33f92d02a9afc3e
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-12-28 17:31:54 +0530

    Taking single line  Input in String  in  JAVA

---
+++ dsa401/conceptofjava/SingleLineStringInput.java
@@ -0,0 +1,38 @@
+// aman
+
+package conceptofjava;
+
+import java.util.Scanner;
+
+public class SingleLineStringInput {
+
+    static void print(String str) {
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        print(str);
+
+    }
+
+}
+// Strings - III -54:29:31
+// Description
+
+// String, such that the size of the string is not given
+
+// Input
+// The first and the only line of the input contains the string
+
+// Output
+// Print the string given as input
+
+// Sample Input 1
+
+// aman
+// Sample Output 1
+
+// aman
