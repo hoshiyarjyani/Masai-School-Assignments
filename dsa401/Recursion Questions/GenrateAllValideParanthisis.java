2fc6d574196fd50d2d34a27d820b0a0ff426a1b8
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-12-29 02:45:39 +0530

    Print Valide Paranthasis ()()() in JAVA

---
+++ dsa401/Recursion Questions/GenrateAllValideParanthisis.java
@@ -0,0 +1,18 @@
+public class GenrateAllValideParanthisis {
+    static void myFun(String s, int open, int close, int n) {
+        if (s.length() == 2 * n) {
+            System.out.println(s);
+            return;
+        }
+        if (open < n) {
+            myFun(s + "(", open + 1, close, n);
+        }
+        if (open > close) {
+            myFun(s + ")", open, close + 1, n);
+        }
+    }
+
+    public static void main(String[] args) {
+        myFun("", 0, 0, 3);
+    }
+}
