a7f38639d4cdcfed96108e2898da32a073d31cd3
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-12-31 02:29:04 +0530

    Power Function Using Recursion Using Input Taking

--- dsa401/Recursion Questions/PowerFunctionRecursion.java
+++ dsa401/Recursion Questions/PowerFunctionRecursion.java
@@ -1,14 +1,38 @@
-public class PowerFunctionRecursion {
-    static int fun(int m, int n) {
-        if (n <= 0) {
+// Power Function - Recursive
+// Description
+
+// Given two integers a and b, we need to find the value of a^b recursively.
+
+// Expected Time Complexity - O(logb).
+
+// Input
+// The first line of the input contains two integers a and b (1 ≤ a ≤ 10) and (0 ≤ b ≤ 9).
+
+// Output
+// For each test case, print the answer: The value of a^b.
+
+// Sample Input 1 
+
+// 2 4
+// Sample Output 1
+
+// 16
+
+import java.util.Scanner;
+
+class PowerFunctionRecursion {
+    static int myFun(int n, int p) {
+        if (p == 0) {
             return 1;
-        } else {
-            return m * fun(m, n - 1);
         }
+        return n * myFun(n, p - 1);
     }
 
     public static void main(String[] args) {
-        int x = fun(2, 5);
-        System.out.println(x);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        int res = myFun(n, p);
+        System.out.println(res);
     }
 }
