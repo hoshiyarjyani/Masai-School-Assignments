7fbd375712a5e2fd943f7e9beb6e98d2939bbf66
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-12-31 02:19:17 +0530

    GCD Recursion Using Input Taking

--- dsa401/Recursion Questions/GCD.java
+++ dsa401/Recursion Questions/GCD.java
@@ -1,14 +1,21 @@
-public class GCD {
-    static int myFun(int x, int y) {
-        if (x == 0) {
-            return y;
-        } else {
-            return myFun(y % x, x);
+import java.util.Scanner;
+
+class GCD {
+    static int myFun(int a, int b) {
+        if (a == 0) {
+            return b;
         }
+        return myFun(b % a, a);
     }
 
     public static void main(String[] args) {
-        int z = myFun(51, 68);
-        System.out.println(z);
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i = 0; i < x; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int z = myFun(a, b);
+            System.out.println(z);
+        }
     }
 }
