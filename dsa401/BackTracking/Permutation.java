147b7cfe0abc702de92f5e8e417d810545909617
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-01-06 03:43:28 +0530

    permutation backtracking in java

--- dsa401/BackTracking/Permutation.java
+++ dsa401/BackTracking/Permutation.java
@@ -2,19 +2,22 @@ package BackTracking;
 
 public class Permutation {
     static void perFun(String str, int l, int r) {
-if(l==r){
-    System.out.println(str);
-}else{
-   for(int i = l;i<=r;i++){
-    str = swap(str,l,i);
-    perFun(str, l+1, r);
-   } 
-}
+        if (l == r) {
+            System.out.println(str);
+        } else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                perFun(str, l + 1, r);
+            }
+        }
     }
 
-    static String swap(String str, int l, int i) {
-       String s =  
-        return null;
+    public static String swap(String str, int i, int j) {
+        char[] chars = str.toCharArray();
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+        return new String(chars);
     }
 
     public static void main(String[] args) {
