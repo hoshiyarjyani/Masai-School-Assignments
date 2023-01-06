98b6598935ccaf802cf2db86887dec1425864855
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-01-07 03:47:49 +0530

    Difference of K by hashMap  in java

---
+++ dsa401/DifferenceOfK_hashMap.java
@@ -0,0 +1,70 @@
+// Difference of K -20:24:20
+// Description
+
+// You are given a sorted arrayAof sizeN, and another integerK
+// You have to find if there exists a pair of integers with indexiandj, such thati != j, andA[i] - A[j] = k
+// If such a pair exists, printYes, else printNo
+
+// Input
+// The first line of the input containsT, the number of test cases.
+
+// The first line of each test case contains N and K, the size of the array and the value of K.
+
+// The next line contains N space separated integers denoting the elements of the array
+
+// Constraints
+
+// 1 <=T<= 10
+
+// 1 <=N<= 1000000
+
+// -10^9 <=A[i]<= 10^9
+
+// 0 <= K <= 10^9
+
+// Output
+// For each test case, print "Yes", if the pair as described in the problem exists, else print "No", on a new line.
+
+// Sample Input 1 
+
+// 2
+// 5 3
+// 1 2 3 4 5
+// 5 8
+// 1 2 3 4 5 
+// Sample Output 1
+
+// Yes
+// No
+// Hint
+
+// In the sample test case, in the first test case, the value ofN = 5, and that ofK = 3. Upon observation, you can see the pairs with values(2, 5)or(1,4)have a difference of 3. Therefore, the output is Yes.
+
+// In the second test case, no pair exists such that the difference between them is 8. Therefore, the output is No.
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            int[] A = new int[N];
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+                set.add(A[i]);
+            }
+            boolean found = false;
+            for (int i = 0; i < N; i++) {
+                if (set.contains(A[i] + K) || set.contains(A[i] - K)) {
+                    found = true;
+                    break;
+                }
+            }
+            System.out.println(found ? "Yes" : "No");
+        }
+    }
+}
