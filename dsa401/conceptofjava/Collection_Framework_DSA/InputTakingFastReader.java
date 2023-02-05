79a58108d7ddaf079f0e4e9ae32e4f9ac07a1898
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-02-05 22:27:30 +0530

    Fast reader input taking

--- dsa401/conceptofjava/Collection_Framework_DSA/InputTakingFastReader.java
+++ dsa401/conceptofjava/Collection_Framework_DSA/InputTakingFastReader.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-
+// input taking from fast reader
 public class Main {
     static class FastReader {
         BufferedReader br;
@@ -51,6 +51,6 @@ public class Main {
 
     public static void main(String[] args) {
         // This will take the input
-        FastReader sc = new FastReader();
+        FastReader  sc = new FastReader();
     }
 }
