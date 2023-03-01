e223822207719ce79a02b0b885eb4c71ba94bcc9
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-03-02 00:37:40 +0530

    Method_Refrence_using_different_method Static,NonStatic,Constructor

---
+++ dsa501/Method_Refrence_using_different_method/MethodRefrence/src/com/masai/Demo.java
@@ -0,0 +1,5 @@
+package com.masai;
+
+public interface Demo {
+	public void printDetail(Student student);
+}
---
+++ dsa501/Method_Refrence_using_different_method/MethodRefrence/src/com/masai/Main.java
@@ -0,0 +1,23 @@
+package com.masai;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student student = new Student("Hoshiyar", 24, "Science");
+		// Using Non-Static Reference
+		Demo demo1 = student::printDetails;
+		demo1.printDetail(student);
+		
+		// Using Static Reference
+		Student student2 = new Student("Ravi", 27, "Maths");
+		Demo demo2 = Student::printStaticDetails;
+		demo2.printDetail(student2);
+		
+		// Using using constructor
+	
+		Student student3 = new Student("Amit", 21, "English");
+		Demo demo3 = Student::new;
+		demo3.printDetail(student3);
+	}
+
+}
