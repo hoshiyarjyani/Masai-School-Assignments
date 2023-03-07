0b6b10641b79eaf6625c0db0a82fb21209e3d13e
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-03-08 00:01:02 +0530

    Multitharading akshay salman hritik thread with join add multiply print name

--- dsa501/Multithred_akshay_salman_hritik_join/Akshay.java
+++ dsa501/Multithred_akshay_salman_hritik_join/Akshay.java
@@ -25,17 +25,24 @@ class Akshay implements Runnable {
 
 	}
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		
 	   Thread t1 = new Thread(new Salman());
 	   Thread t2 = new Thread(new Hritik());
 	   Thread t3 = new Thread(new Akshay(),"Akshay");
 	   t1.start();
+	   try {
+		t1.join();
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
 	   t2.start();
-	   t3.start();	 
-	   
-	    
-	   t3.join();
+	   try {
+		t2.join();
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+	   t3.start();
 	}
 	
 
