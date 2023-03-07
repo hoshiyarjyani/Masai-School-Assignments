840014553860f5ddec0647c56f82702f3cd222c1
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-03-07 23:43:12 +0530

    Multitharading akshay salman hritik thread with join add multiply print name

---
+++ dsa501/Multithred_akshay_salman_hritik_join/Akshay.java
@@ -0,0 +1,43 @@
+//1. **Create a Multithreaded application to start 3 threads :**
+//- **Salman Thread**
+//- **Hritik Thread**
+//- **Akshay Thread should be the main thread**
+//
+//**Salman thread will print the sum of the first 10 number**
+//
+//**Hritik thread will print the multiplication of the first 10 number**
+//
+//**Akshay thread will print its name 10 times.**
+//
+//***Tip: Use Runnable interface to implement the same. use join() 
+//method to make sure that main thread must finish after all child threads.***
+package jyani.hoshiyar.problem1;
+
+class Akshay implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(Thread.currentThread().getName() + " Thread");
+		}
+
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+	   Thread t1 = new Thread(new Salman());
+	   Thread t2 = new Thread(new Hritik());
+	   Thread t3 = new Thread(new Akshay(),"Akshay");
+	   t1.start();
+	   t2.start();
+	   t3.start();	 
+	   
+	   t1.join();
+	   t2.join();
+	   t3.join();
+	}
+	
+
+}
---
+++ dsa501/Multithred_akshay_salman_hritik_join/Hritik.java
@@ -0,0 +1,28 @@
+//1. **Create a Multithreaded application to start 3 threads :**
+//- **Salman Thread**
+//- **Hritik Thread**
+//- **Akshay Thread should be the main thread**
+//
+//**Salman thread will print the sum of the first 10 number**
+//
+//**Hritik thread will print the multiplication of the first 10 number**
+//
+//**Akshay thread will print its name 10 times.**
+//
+//***Tip: Use Runnable interface to implement the same. use join() 
+//method to make sure that main thread must finish after all child threads.***
+
+package jyani.hoshiyar.problem1;
+class Hritik implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		int mul = 1;
+		for(int i=1;i<=10;i++) {
+			mul*=i;
+		}
+		System.out.println(mul);
+	}
+	
+}
