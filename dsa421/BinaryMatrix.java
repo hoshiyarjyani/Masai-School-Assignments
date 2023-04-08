5e604e10d8ca5689b0d75a18b7549adfb565d6af
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-04-08 11:16:56 +0530

    Binary Matrix

---
+++ dsa421/BinaryMatrix.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class BinaryMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int k = sc.nextInt();
+                if (k == 1) {
+                    arr[i][j] = 0;
+                } else {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
