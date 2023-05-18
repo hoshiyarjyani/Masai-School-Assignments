dc269a4b3e4967986940abb45e76681bf18edd58
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-18 22:35:32 +0530

    ac ques

---
+++ RJ101/AC_Getting_heated.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int a = sc.nextInt(); // 30 30 30 30
+            int b = sc.nextInt(); // 35 35 35 25
+            int c = sc.nextInt(); // 25 40 35 35
+
+            if (a <= b && (c <= a || c <= b)) {
+                System.out.println("AC");
+            } else {
+                System.out.println("No AC");
+            }
+
+        }
+    }
+}
