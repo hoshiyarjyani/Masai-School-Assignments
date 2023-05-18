977ccf6f2e389346ac71493f7b8c9440286b46f4
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-18 22:46:47 +0530

    LongestSubarrayWithSumLessThanOrEqualToK

---
+++ RJ101/LongestSubarrayWithSumLessThanOrEqualToK.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int maxlen = 0;
+            int st = 0;
+            int sum = 0;
+            int len = 0;
+            for (int i = 0; i < n; i++) {
+                sum += arr[i];
+                while (sum > k) {
+                    sum = sum - arr[st];
+                    st++;
+                }
+
+                len = i - st + 1;
+                maxlen = Math.max(maxlen, len);
+
+            }
+            System.out.println(maxlen);
+
+        }
+    }
+}
