bf89368ff16828f6be96b1d6b3665387595a4cf7
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-18 22:42:30 +0530

    Jumping_on_the_clouds

---
+++ RJ101/Jumping_on_the_clouds.java
@@ -0,0 +1,76 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        // This will take the input
+        FastReader sc = new FastReader();
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int jump = 0;
+            int k = 0;
+            while (k < n - 1) {
+                if (k + 2 < n && arr[k + 2] == 0) {
+                    k += 2;
+                } else {
+                    k++;
+                }
+                jump++;
+            }
+            System.out.println(jump);
+        }
+    }
+
+}
