29a5ab9230207bb7efd067c00413d95bcd30a611
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-19 11:45:18 +0530

    Rotate The Array Given Times

---
+++ RJ101/RotateTheArrayGivenTimes.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class RotateTheArrayGivenTimes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int t = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                int newPos = (i + t) % n;
+                res[newPos] = arr[i];
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
