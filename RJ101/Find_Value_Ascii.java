54050670cf5d62b0abb593d62edc8c0b98f52d23
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-20 23:36:43 +0530

    Find Value

---
+++ RJ101/Find_Value_Ascii.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+
+        int min = 0;
+        int plus = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                int x = ch - 'a' + 1;
+                min += x;
+            } else if (ch >= 'A' && ch <= 'Z') {
+                int y = 'A' - ch - 1;
+                plus += y;
+            }
+        }
+
+        System.out.println(min + plus);
+    }
+}
+// input = abABX
+// output= -24
