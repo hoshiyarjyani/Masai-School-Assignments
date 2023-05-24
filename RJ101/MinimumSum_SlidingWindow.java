26e80ecfa8b7241871fac513f9e2039ca966b352
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-24 16:51:51 +0530

    MinimumSum_SlidingWindow

---
+++ RJ101/MinimumSum_SlidingWindow.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class MinimumSum_SlidingWindow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int minSum = 0;
+            for (int i = 0; i < k; i++) {
+                minSum += arr[i];
+            }
+
+            int sum = minSum;
+            for (int i = k; i < n; i++) {
+                sum += arr[i] - arr[i - k];
+                minSum = Math.min(minSum, sum);
+            }
+
+            System.out.println(minSum);
+
+        }
+    }
+}
