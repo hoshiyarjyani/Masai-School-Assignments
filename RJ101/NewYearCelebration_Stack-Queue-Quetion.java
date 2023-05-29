11377e4238df1304c99fcd82329aee8d22424f31
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-29 17:00:31 +0530

    New Year Celebration Stack And Queue Quetion

---
+++ RJ101/NewYearCelebration_Stack-Queue-Quetion.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> stk = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        while (n-- > 0) {
+            int x = sc.nextInt();
+            if (x == 1) {
+                queue.add(sc.nextInt());
+            } else if (x == 2) {
+                stk.push(sc.nextInt());
+            } else if (x == 3) {
+                System.out.println(queue.isEmpty() ? -1 : queue.peek());
+            } else if (x == 4) {
+                System.out.println(stk.isEmpty() ? -1 : stk.peek());
+            } else if (x == 5) {
+                if (!queue.isEmpty()) {
+                    stk.push(queue.poll());
+                }
+            }
+        }
+    }
+}
