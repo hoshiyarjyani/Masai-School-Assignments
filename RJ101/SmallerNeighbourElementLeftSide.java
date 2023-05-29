dc50fab8c2b25c1d6bb817cc815e2ffab75a6959
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-29 16:49:59 +0530

    Smaller Neighbour Element Left Side

---
+++ RJ101/SmallerNeighbourElementLeftSide.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int ans[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+
+    }
+}
