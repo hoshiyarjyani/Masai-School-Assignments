eaf6b211687ab3b930ad074648462626caf6d6d2
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-29 16:57:43 +0530

    Bracket is Balanced or not

---
+++ RJ101/Againest_A_ClassicalProblem_Breackt_Blanceing.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            String str = sc.next();
+            boolean check = true;
+            if (str.length() % 2 == 1) {
+                System.out.println("not balanced");
+            } else {
+                Stack<Character> stack = new Stack<>();
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{') {
+                        stack.push(str.charAt(i));
+                    } else {
+                        if (stack.isEmpty()) {
+                            check = false;
+                            break;
+                        }
+                        char top = stack.pop();
+                        if ((str.charAt(i) == ')' && top != '(') ||
+                                (str.charAt(i) == ']' && top != '[') ||
+                                (str.charAt(i) == '}' && top != '{')) {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+
+                System.out.println(check ? "balanced" : "not balanced");
+            }
+        }
+    }
+}
