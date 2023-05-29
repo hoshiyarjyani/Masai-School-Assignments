7b103fa5273923953fb748b5008a29fefc765dc9
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-29 16:52:28 +0530

    Next Greater Element Right Side

---
+++ RJ101/NextGreaterElementRightSide.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Stack<Integer> stack = new Stack<>();
+
+            int ans[] = new int[n];
+
+            for (int i = n - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                    stack.pop();
+                }
+                if (stack.isEmpty()) {
+                    ans[i] = -1;
+                } else {
+                    ans[i] = stack.peek();
+                }
+                stack.push(arr[i]);
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
