f51e006a747dff4770ee6af29adb5b153919a97c
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-03 18:07:40 +0530

    Count transformations

---
+++ RJ101/CountTransformations.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int x = 0;
+            System.out.println(fun(a, b, x));
+
+        }
+    }
+
+    static int fun(int a, int b, int x) {
+        if (a == b) {
+            return x;
+        }
+        if (a < b) {
+            return fun(a, b - a, x + 1);
+        } else {
+            return fun(a - b, b, x + 1);
+        }
+    }
+
+}
