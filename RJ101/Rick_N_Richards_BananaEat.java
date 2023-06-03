8686d0c411eb7a165c04ae717da007421a428f26
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-03 10:49:56 +0530

    Rick And Richards

---
+++ RJ101/Rick_N_Richards_BananaEat.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int rickSum = 0;
+            int chirdSum = 0;
+            int banana1 = 0;
+            int banana2 = 0;
+            int i = 0;
+            int j = n - 1;
+            while (i <= j) {
+                if (rickSum <= 2 * chirdSum) {
+                    rickSum += arr[i];
+                    i++;
+                    banana1++;
+                } else {
+                    chirdSum += arr[j];
+                    j--;
+                    banana2++;
+                }
+            }
+            System.out.println(banana1 + " " + banana2);
+        }
+    }
+}
