91fe8070ecf0cc7f8a16c8ccbb195f007c1e66f6
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-03 10:53:31 +0530

    Can we make target number

---
+++ RJ101/AtoB_Traget_NumberIs_Possible_Or_Not.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int tar = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int ans = myFun(s, tar, x, y);
+        System.out.println(ans);
+    }
+
+    static int myFun(int s, int tar, int x, int y) {
+        if (s == tar) {
+            return 0;
+        }
+        if (s > tar) {
+            return -1;
+        }
+        int dpx = myFun(s * x, tar, x, y);
+        int dpy = myFun(s * y, tar, x, y);
+
+        if (dpx == -1 && dpy == -1) {
+            return -1;
+        }
+        if (dpy == -1) {
+            return dpx + 1;
+        }
+        if (dpx == -1) {
+            return dpy + 1;
+        }
+        int ans = Math.min(dpx, dpy);
+        return ans + 1;
+    }
+}
