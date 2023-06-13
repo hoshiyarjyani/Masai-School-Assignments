83372ab2249af6212b90e5caf95dd29b1a99ea2e
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-13 17:14:01 +0530

    Panagram or not

---
+++ RJ101/HackerRank/PanagramOrNot_A_String_That_Contains_All_Alphabats.java
@@ -0,0 +1,97 @@
+// A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+
+// Example
+
+// The string contains all letters in the English alphabet, so return pangram.
+
+// Function Description
+
+// Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+
+// pangrams has the following parameter(s):
+
+// string s: a string to test
+// Returns
+
+// string: either pangram or not pangram
+// Input Format
+
+// A single line with string .
+
+// Constraints
+
+// Each character of , 
+
+// Sample Input
+
+// Sample Input 0
+
+// We promptly judged antique ivory buckles for the next prize
+
+// Sample Output 0
+
+// pangram
+
+// Sample Explanation 0
+
+// All of the letters of the alphabet are present in the string.
+
+// Sample Input 1
+
+// We promptly judged antique ivory buckles for the prize
+
+// Sample Output 1
+
+// not pangram
+
+// Sample Explanation 0
+
+// The string lacks an x.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+        Set<Character> charSet = new HashSet<>();
+        for (char ch : s.toCharArray()) {
+            // Ignore non-alphabetic characters
+            if (Character.isLetter(ch)) {
+                charSet.add(Character.toLowerCase(ch));
+            }
+        }
+
+        return charSet.size() == 26 ? "pangram" : "not pangram";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
