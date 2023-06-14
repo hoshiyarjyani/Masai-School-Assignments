5d3c19d9a960330db087016ea176a08f7e531331
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-14 11:30:12 +0530

    Opraton requird for making Palindromic substring

--- RJ101/HackerRank/TheLove-LetterMystery_Make-Palindrome-Count.java
+++ RJ101/HackerRank/TheLove-LetterMystery_Make-Palindrome-Count.java
@@ -27,7 +27,6 @@
 
 // Constraints
 
-
 //  | s | 
 // All strings are composed of lower case English letters, ascii[a-z], with no spaces.
 
@@ -53,4 +52,59 @@
 // For the third query, abcd → abcc → abcb → abca → abba.
 // For the fourth query, cba → bba → aba.
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
+class Result {
+
+    /*
+     * Complete the 'theLoveLetterMystery' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int theLoveLetterMystery(String s) {
+
+        int count = 0;
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            count += Math.abs(s.charAt(i) - s.charAt(j));
+            i++;
+            j--;
+        }
+
+        return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = bufferedReader.readLine();
+
+            int result = Result.theLoveLetterMystery(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
