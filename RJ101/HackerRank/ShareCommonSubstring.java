620041c76d2fd783411e0e28e83872214e8839f8
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-15 19:27:48 +0530

    common substrings

--- RJ101/HackerRank/ShareCommonSubstring.java
+++ RJ101/HackerRank/ShareCommonSubstring.java
@@ -2,10 +2,7 @@
 
 // Example
 
-
 // These share the common substring .
-
-
 
 // These do not share a substring.
 
@@ -52,3 +49,74 @@
 
 // , . The substrings  and  are common to both strings.
 // , .  and  share no common substrings.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     * 1. STRING s1
+     * 2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+        // Write your code here
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s1.length(); i++) {
+            set.add(s1.charAt(i));
+        }
+        boolean check = false;
+
+        for (int i = 0; i < s2.length(); i++) {
+            if (set.contains(s2.charAt(i))) {
+                check = true;
+                break;
+            }
+        }
+
+        return check ? "YES" : "NO";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
