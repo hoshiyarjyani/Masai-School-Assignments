08eaaca758743983613b5219ffaadcb8f451b77b
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-16 10:54:36 +0530

    Check HackerRank is present or not in a string

--- RJ101/HackerRank/FindWordInString_HackerRank-In-String.java
+++ RJ101/HackerRank/FindWordInString_HackerRank-In-String.java
@@ -8,9 +8,7 @@
 
 // This contains a subsequence of all of the characters in the proper order. Answer YES
 
-
 // This is missing the second 'r'. Answer NO.
-
 
 // There is no 'c' after the first occurrence of an 'a', so answer NO.
 
@@ -44,7 +42,6 @@
 
 // We perform the following  queries:
 
-
 // The characters of hackerrank are bolded in the string above. Because the string contains all the characters in hackerrank in the same exact order as they appear in hackerrank, we return YES.
 //  does not contain the last three characters of hackerrank, so we return NO.
 // Sample Input 1
@@ -57,4 +54,43 @@
 // YES
 // NO
 
+import java.io.*;
+import java.util.*;
 
+class Result {
+    public static String hackerrankInString(String s) {
+        String target = "hackerrank";
+        int n = target.length();
+        int i = 0, j = 0;
+
+        while (i < n && j < s.length()) {
+            if (target.charAt(i) == s.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return (i == n) ? "YES" : "NO";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = bufferedReader.readLine();
+
+            String result = Result.hackerrankInString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
