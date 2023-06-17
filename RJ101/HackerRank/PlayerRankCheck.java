237fe0e385abcf9c1cdd76645e4547c3c6e7a713
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-17 09:34:58 +0530

    Player Rank CHeck

--- RJ101/HackerRank/PlayerRankCheck.java
+++ RJ101/HackerRank/PlayerRankCheck.java
@@ -4,8 +4,6 @@
 // Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
 // Example
 
-
-
 // The ranked players will have ranks , , , and , respectively. If the player's scores are ,  and , their rankings after each game are ,  and . Return .
 
 // Function Description
@@ -48,17 +46,12 @@
 // 20
 // 10
 
- 
-
-
-
 // Array: player
 // 5
 // 25
 // 50
 // 120
 
- 
 // 7
 // 100 100 50 40 40 20 10
 // 4
@@ -91,7 +84,6 @@
 
 // image
 
-
 // Sample Input 2
 
 // CopyDownload
@@ -103,10 +95,6 @@
 // 75
 // 60
 
- 
-
-
-
 // Array: player
 // 50
 // 65
@@ -114,7 +102,6 @@
 // 90
 // 102
 
- 
 // 6
 // 100 90 90 80 75 60
 // 5
@@ -127,4 +114,60 @@
 // 2
 // 1
 
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
+class Result {
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+
+        List<Integer> res = new ArrayList<>();
+
+        List<Integer> uniqueRanked = ranked.stream().distinct().collect(Collectors.toList());
+        
+        int n = uniqueRanked.size();
+        int m = player.size();
+        int i = n - 1;
+
+        for (int j = 0; j < m; j++) {
+            while (i >= 0 && player.get(j) >= uniqueRanked.get(i)) {
+                i--;
+            }
+            res.add(i + 2);
+        }
+
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ranked = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int playerCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> player = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> result = Result.climbingLeaderboard(ranked, player);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining("\n"))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
