c67e2ca37fa77a1fc2e21b6904e906a20194708e
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-17 11:42:48 +0530

    How to use Big Int in java

--- RJ101/HackerRank/Factorial_BigIntConceptUse.java
+++ RJ101/HackerRank/Factorial_BigIntConceptUse.java
@@ -1,1 +1,35 @@
 https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static void extraLongFactorials(int n) {
+        BigInteger res = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            res = res.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(res);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
