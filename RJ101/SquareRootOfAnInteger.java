71410d80fa8d9447a782f8f5d0843fef21236036
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-07-03 17:38:40 +0530

    Squre root of an Integer without inbuilt method

--- RJ101/SquareRootOfAnInteger.java
+++ RJ101/SquareRootOfAnInteger.java
@@ -25,6 +25,47 @@ class Main {
     }
 }
 
+Other Approch-
+
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int ans = calculateSquareRoot(n);
+            System.out.println(ans);
+        }
+    }
+
+    private static int calculateSquareRoot(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int start = 1;
+        int end = n;
+        int ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid <= n / mid) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
+
+
 
 Sample Input
 2
