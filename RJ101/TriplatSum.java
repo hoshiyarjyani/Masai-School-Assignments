ebb17d7b04ab947006451aa84aa65455f0819656
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-07-03 17:22:21 +0530

    Triplet sum, two number sum is present or not in list

---
+++ RJ101/TriplatSum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                list.add(sc.nextInt());
+            }
+            boolean check = false;
+            Collections.sort(list);
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (list.contains(list.get(i) + list.get(j))) {
+                        check = true;
+                        break;
+                    }
+                }
+                if (check) {
+                    break;
+                }
+            }
+            System.out.println(check ? "1" : "0");
+        }
+
+    }
+}
